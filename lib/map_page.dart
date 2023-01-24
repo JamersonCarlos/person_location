1ab83780c3f35e7a79e1183fd3b665f5a10dc3be
Jamerson Carlos <64469941+JamersonCarlos@users.noreply.github.com>
2023-01-24 19:00:10 -0300

    markers according to current position

--- lib/map_page.dart
+++ lib/map_page.dart
@@ -1,33 +1,92 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:personal_location/models/LocationModel.dart';
+import 'package:personal_location/repositories/Database.dart';
 
 class MyMap extends StatefulWidget {
-  const MyMap({super.key});
+  LatLng? initialPosition;
+  MyMap({super.key, this.initialPosition});
 
   @override
   State<MyMap> createState() => _MyMapState();
 }
 
 class _MyMapState extends State<MyMap> {
+  List<dynamic>? locationsModel;
+  List<LatLng> testeTappedPoints = [];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    DBprovider.db.AllLocations().then((value) {
+      setState(() {
+        locationsModel = value;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    var markers = locationsModel != null
+        ? locationsModel!.map((value) {
+            return Marker(
+                height: 80,
+                width: 80,
+                point: LatLng(value["latitude"], value["longitude"]),
+                builder: (ctx) => Column(
+                      children: [
+                        Text(
+                          value["nomeMarcador"],
+                          style: const TextStyle(
+                            color: Colors.red,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Container(
+                          child: const Icon(
+                            Icons.pin_drop,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ],
+                    ));
+          }).toList()
+        : null;
+
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          await DBprovider.db.deletAllLocations();
+          setState(() {
+            DBprovider.db
+                .AllLocations()
+                .then((value) => locationsModel = value);
+          });
+        },
+        child: const Icon(
+          Icons.delete,
+          color: Colors.white,
+        ),
+      ),
       appBar: AppBar(
         title: const Text("Flutter Map"),
         centerTitle: true,
       ),
       body: FlutterMap(
         options: MapOptions(
-          center: LatLng(-8.42224563395907, -37.05453387019272),
+          center: widget.initialPosition,
           zoom: 17,
         ),
         layers: [
           TileLayerOptions(
             urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-          )
+          ),
+          locationsModel != null
+              ? MarkerLayerOptions(markers: markers!)
+              : MarkerLayerOptions(),
         ],
       ),
     );
