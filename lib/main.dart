f61a223f7e9c3aaf3fa467ffe60f2292ee3586e1
Jamerson Carlos <64469941+JamersonCarlos@users.noreply.github.com>
2023-01-24 00:51:38 -0300

    material app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:personal_location/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,6 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Flutter Demo',
+      home: HomeApp(),
     );
   }
 }
